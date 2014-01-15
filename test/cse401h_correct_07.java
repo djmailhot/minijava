c256cd7f0dc98f0d32adb2150b2ec57ae16bd7d8
Jake Bailey <rjacob@cs.washington.edu>
2014-01-15 10:49:13 -0800

    Add test for arithmetic operators on doubles.

---
+++ test/cse401h_correct_07.java
@@ -0,0 +1,39 @@
+//
+// Test arithmetic operators on doubles
+//
+class cse401h_correct_07 {
+
+  public static void main(String[] args) {
+    System.out.println(new TestDoubles().run());
+  }
+
+}
+
+class TestDoubles {
+
+  public int run() {
+    double x;
+
+    x = 5.0 + 3.0;
+    System.out.println(x);
+    x = 5.0 - 3.0;
+    System.out.println(x);
+    x = 4.0 / 2.0;
+    System.out.println(x);
+    x = 5.0 / 2.0;
+    System.out.println(x);
+    x = 5.0 * 2.0;
+    System.out.println(x);
+    x = 5.5 * 2.0;
+    System.out.println(x);
+    x = 5.0 % 3.0;
+    System.out.println(x);
+    x = 5.2 % 3.0;
+    System.out.println(x);
+    x = 5.0 % 3.5;
+    System.out.println(x);
+
+    return 0;
+  }
+
+}

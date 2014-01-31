2c220ab97ded70923e4ad48c8bce3247d5615c69
David Mailhot <djmailhot@gmail.com>
2014-01-30 16:35:47 -0800

    Parser:  correct_11 test now tests length, parser parses correctly

--- test/cse401h_correct_11.java
+++ test/cse401h_correct_11.java
@@ -15,8 +15,9 @@ class TestArrays{
     int[] a;
     int[] b;
     int[] c;
+    int x;
 
-    System.out.println(100000011);
+    System.out.println(100000012);
 
 
     // Construction
@@ -27,44 +28,40 @@ class TestArrays{
 
     // Assignment
     b[1] = 1;
-    b[2] = 2;
-    c[11] = 3;
-    c[99] = 4;
-    b[7] = 7;
-    b[8] = 8;
+    c[11] = 2;
+    c[99] = 3;
+    b[4] = 4;
+    b[5] = 5;
     
 
     // Access
     System.out.println(this.checkValue(b[0], 0));
     System.out.println(this.checkValue(b[1], 1));
-    System.out.println(this.checkValue(b[2], 2));
-    System.out.println(this.checkValue(c[11], 3));
-    System.out.println(this.checkValue(c[99], 4));
-
-
-    // Reassignment
-    b[0] = 5;
-    b[1] = 6;
-    System.out.println(this.checkValue(b[0], 5));
-    System.out.println(this.checkValue(b[1], 6));
+    System.out.println(this.checkValue(c[11], 2));
+    System.out.println(this.checkValue(c[99], 3));
 
 
     // Object semantics
     a = b;
-    System.out.println(this.checkValue(a[7], b[7]));
-    System.out.println(this.checkValue(a[8], b[8]));
+    System.out.println(this.checkValue(a[4], b[4]));
+    System.out.println(this.checkValue(a[5], b[5]));
 
-    a[0] = 9;
-    System.out.println(this.checkValue(a[0], 9)); // change a, check a
-    a[1] = 10;
-    System.out.println(this.checkValue(b[1], 10)); // change a, check b
-    b[2] = 11;
-    System.out.println(this.checkValue(b[2], 11)); // change b, check b
-    b[3] = 12;
-    System.out.println(this.checkValue(a[3], 12)); // change b, check a
+    a[0] = 6;
+    System.out.println(this.checkValue(a[0], 6)); // change a, check a
+    a[1] = 7;
+    System.out.println(this.checkValue(b[1], 7)); // change a, check b
+    b[2] = 8;
+    System.out.println(this.checkValue(b[2], 8)); // change b, check b
+    b[3] = 9;
+    System.out.println(this.checkValue(a[3], 9)); // change b, check a
 
 
-    return 13;
+    // Length
+    x = b.length;
+    System.out.println(x);
+
+
+    return 11;
   }
 
   public int checkValue(int x, int y) {

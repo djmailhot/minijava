470bd32c41cbafafdfbb0bb35fa63be57a6fd6dd
Jake Bailey <rjacob@cs.washington.edu>
2014-02-26 15:17:15 -0800

    Codegen: Clean up CodeGenMain.

--- src/CodeGenMain.java
+++ src/CodeGenMain.java
@@ -1,3 +1,8 @@
+/*
+ * Group cse401h
+ * Jake Bailey, David Mailhot
+ */
+
 import AST.*;
 import CodeGenerator.*;
 import Parser.parser;
@@ -5,45 +10,27 @@ import Scanner.scanner;
 
 import java_cup.runtime.Symbol;
 
-import java.util.*;
-
 /**
- * Entry point for code generation. Feel free to use this as your compiler's
- * entry point, and feel free to modify it as you see fit.
+ * Entry point for code generation.
  */
 public class CodeGenMain {
   public static void main(String[] args) {
     try {
-      //
-      // create a scanner on the input file
-      //
+      // create a scanner on the input file and parse it
       scanner s = new scanner(System.in);
       parser p = new parser(s);
-      // print to stdout
+      Symbol root = p.parse();
+      Program program = (Program) root.value;
+
       CodeGenerator cg = new CodeGenerator(null);
-      Symbol root;
-
-      //
-      // replace p.parse() with p.debug_parse() in next line to see trace of
-      // parser shift/reduce actions during parse
-      //
-      root = p.parse();
-      Program program = (Program)root.value;
       program.accept(new CodeGeneratorVisitor(cg));
-      //
-      // System.out.print("\n" + "Parsing completed");
-      //
-    } catch (Exception e) {
-      //
-      // yuck: some kind of error in the compiler implementation
-      // that we're not expecting (a bug!)
-      //
-      System.err.println("Unexpected internal compiler error: " + e.toString());
 
-      //
-      // print out a stack dump
-      //
+      System.exit(0);
+
+    } catch (Exception e) {
+      System.err.println("Unexpected internal compiler error: " + e.toString());
       e.printStackTrace();
+      System.exit(1);
     }
   }
 }
